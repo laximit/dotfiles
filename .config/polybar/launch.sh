b386ddd1137f684c6b4bfbe90731765aba445cf5
Murilo Lobos <murilolobos48@gmail.com>
2022-06-11 17:07:26 +0000

    [EDIT] Removed smart_gaps and hide_edge_borders from i3 config | [CREATED] Added polybar config (Dock and Statusbar)

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+killall -q polybar
+
+echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+polybar statusbar 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar appmenu 2>&1 | tee -a /tmp/polybar2.log & disown
